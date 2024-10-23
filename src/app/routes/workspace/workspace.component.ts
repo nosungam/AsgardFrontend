c2a045024273c82104ddcf951d47f8d7c58d4a69
Matías Presuttari <matipresuttari@gmail.com>
2024-10-23 16:01:28 -0300

    Workshop screen cambios

--- src/app/routes/workspace/workspace.component.ts
+++ src/app/routes/workspace/workspace.component.ts
@@ -8,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './workspace.component.css'
 })
 export class WorkspaceComponent {
-
+  folders = [
+    {
+      name: 'Folder 1',
+    },
+    {
+      name: 'Folder 2',
+    },
+    {
+      name: 'Folder 3',
+    }
+  ];
+  flashcards = [
+    {
+      image: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Spain.svg',
+      question: 'What is the capital of Spain?',
+      description: 'This is a description',
+      answer: 'Madrid',
+    },
+    {
+      image: '',
+      question: 'What is the capital of France?',
+      description: 'This is a description',
+      answer: 'Paris',
+    },
+    {
+      image: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg',
+      question: 'What is the capital of Italy?',
+      answer: 'Rome',
+      description: 'This is a description',
+    }
+  ];
 }
