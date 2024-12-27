37db37acf1f170248615a0a4e44864dc5a22103c
Matías Presuttari <matipresuttari@gmail.com>
2024-12-27 11:38:25 -0300

    study-session fix

--- src/app/routes/study-session/study-session.component.ts
+++ src/app/routes/study-session/study-session.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-study-session',
@@ -13,10 +13,19 @@ import { Router } from '@angular/router';
 export class StudySessionComponent {
   workspaceId: number | null = null;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   stopSession(){
-    this.workspaceId = paramMap.get("id") ? parseInt(paramMap.get("id")!, 10) : null;
-    this.router.navigate(['/session', this.workspaceId]);
+    this.route.paramMap.subscribe(paramMap => {
+      this.workspaceId = paramMap.get("id") ? parseInt(paramMap.get("id")!, 10) : null;
+      this.router.navigate(['/workspace/', this.workspaceId]);
+    });
+  }
+
+  nextQuestion(){
+    this.route.paramMap.subscribe(paramMap => {
+      this.workspaceId = paramMap.get("id") ? parseInt(paramMap.get("id")!, 10) : null;
+      this.router.navigate(['/session/', this.workspaceId]);
+    });
   }
 }
