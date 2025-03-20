7a77c631abc675e5acebc0b135e52c652ba68e69
nosungam <106203325+nosungam@users.noreply.github.com>
2025-03-20 15:10:02 -0300

    credentials are invalid

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -14,6 +14,7 @@ import { CommonModule } from '@angular/common';
 export class LoginComponent {
   showHide= 'show';
   typeShowHide = 'password';
+  invalidLogin = false;
 
   form = this.formBuilder.group({
     password: ["", [Validators.required, Validators.minLength(6)]],
@@ -33,12 +34,14 @@ export class LoginComponent {
         email: this.form.value.email!,
         name: ''
       };
+      
       this.authService.login(loginData)
       .then(() => {
         this.router.navigate(['/home']);
         resolve(true);
       })
       .catch((error)=>{ 
+        this.invalidLogin = true;
         console.log(error);
         reject(error);
       })})
